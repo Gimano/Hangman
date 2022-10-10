7188d502dd8efd5fdca58d20487d0a0b5385b99c
Gimano <112472723+Gimano@users.noreply.github.com>
2022-10-10 12:10:46 +0200

    La till menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿while (true)
+{
+    Console.WriteLine("Välkommen till Hangman");
+    Console.WriteLine("1. Spela\n2. Avsluta");
+    int choice = int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            //RunGame();
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+}

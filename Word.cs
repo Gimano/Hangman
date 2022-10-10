0d5bd6b531d52e565e27ce3bc63c718d52e68bae
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-10 14:14:48 +0200

    array word

---
+++ Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Word
+    {
+        public static void HangmanWord() 
+        {
+             string[] KategoriHuvudstaden =
+            {
+            "Stockholm",
+            "Tokyo",
+            "Honolulu",
+            "Beijing",
+            "Washington DC",
+            "Canberra",
+            "Budapest",
+            "Helsinki",
+            "Ottowa",
+            "Soeul",
+            }; 
+        }
+    }
+
+}

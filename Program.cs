c28522b0320e05501b7a918313d7647a5ff44e66
Gimano <112472723+Gimano@users.noreply.github.com>
2022-10-10 15:30:36 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿bool game= true;
+﻿using Hangman;
+
+bool game= true;
 while (game)
 {
+    //Console.Clear();
     Console.WriteLine("Välkommen till Hangman");
     Console.WriteLine("1. Spela\n2. Avsluta");
     int choice = int.Parse(Console.ReadLine());
     switch (choice)
     {
         case 1:
-            //RunGame();
+            Guess.RunGame();
             break;
         case 2:
             game = false;
